47069d7b87471cab6239841f0e869845e1a42d93
go <rudotcom@gmail.com>
2020-12-08 05:20:29 +0300

    Голосовой помощник

--- test.py
+++ test.py
@@ -12,20 +12,3 @@ connection = pymysql.connect('localhost', 'dude', 'StqMwx4DRdKrc6WWGcw2w8nZh', '
     # # your changes.
     # connection.commit()
 
-import random,os,sys
-
-folder=os.listdir(os.getcwd())
-file=random.choice(folder)
-ext3=['.mp3']
-print('First random pick: '+file)
-
-while file[-4:] not in ext3 :
-    print('Not an MP3 file  : '+file)
-    file=random.choice(folder)
-else:
-    os.startfile(file)
-    print('Song name: '+file)
-
-sys.exit()
-
-##os.startfile(random.choice(folder))
